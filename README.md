c30ba3c1c7b50bad034eb0ebea07b188fcc5cfc6
sriram23 <sriramsubramanian23@gmail.com>
2019-10-15 09:53:02 +0530

    Fixed bugs

--- README.md
+++
@@ -1,9 +0,0 @@
-# Battery Tracker for Linux
-
-This is a simple battery charge tracker that tracks the battery status and alerts the user when battery is low (<30%) when discharging and when battery is full when charging.
-
-### How to install?
-
-- Download the zip file and extract any where in your system
-- Open terminal in that location and run <i>install.sh</i> shell script.
-- The battery tracker will start running in background
--- install.sh
+++ install.sh
@@ -1,4 +1,21 @@
 echo $(touch PowerTracker.desktop)
+tracker=$PWD/tracker.sh
+tracker_txt=$PWD/tracker.txt
+
+echo "dir=$PWD" > $tracker
+# echo $(cat $tracker_txt)
+# echo $(cat $tracker_txt) >> $tracker
+# echo $(cp -n $tracker_txt $tracker)
+# sudo cp -vnpr /xxx/* /yyy
+
+while IFS= read -r line
+do
+# echo "$line"
+echo "$line\n" >>$tracker
+
+done <"$tracker_txt"
+
+
 my_desktop=$PWD/PowerTracker.desktop
 echo "[Desktop Entry]" > $my_desktop
 echo "Type=Application" >> $my_desktop
@@ -7,6 +24,12 @@ echo "Icon=$PWD/battery.png" >> $my_desktop
 echo "Name=Power Tracker" >> $my_desktop
 echo "Terminal=false" >> $my_desktop
 echo "X-GNOME-Autostart-enabled=true" >> $my_desktop
-echo $(sudo chmod 775 *)
+echo $(sudo chmod 777 *)
+[ -d ~/.config/autostart ] && echo "Auto start enabled..." || echo $(sudo mkdir ~/.config/autostart) || echo "Auto start enabled..."
 echo $(sudo cp PowerTracker.desktop ~/.config/autostart/)
-echo "<<<<<<<<<<<<<<<Successfully Installed!>>>>>>>>>>>>>>>>"
+# echo $(~/.config/autostart/PowerTracker.desktop)
+echo "If there is no error messages, reboot your machine to run battery tracker"
+echo "Instead, you can run by double clicking \"PowerTracker\" icon"
+# [ -d ~/.config/autostart/PowerTracker.desktop ] && echo "Installed Successfully!" || echo "Installation Failed"
+# echo "If this you do not see any messages above, then the battery tracker is installed :-)"
+# echo "Else if, you got some messages, we are sorry to say Battery saver is not installed :-("
